93ff78782a739241889875afb6b531b764bb7a67
Sam Unwin <unwin100@gmail.com>
2020-03-21 22:07:17 +0000

    added keras models to train on data

--- train.py
+++ train.py
@@ -15,16 +15,41 @@ def convert_videos_to_json(path_in, path_out):
               '--checkpoint  pretrained_models/fast_421_res152_256x192.pth --video ' + path_in + ' --outdir ' + path_out + ' --sp')
 
 
-prepare_dataset()
-os.chdir("AlphaPose")
+#prepare_dataset()
+#os.chdir("AlphaPose")
 
-for i in range(1, 1001):
-    file_in = str("../dataset/Violence/V_") + str(i) + ".mp4"
-    file_out = str("../dataset/results/V_") + str(i) + "_json"
-    convert_videos_to_json(file_in, file_out)
+# for i in range(1, 1001):
+#    file_in = str("../dataset/Violence/V_") + str(i) + ".mp4"
+#    file_out = str("../dataset/results/V_") + str(i) + "_json"
+#    convert_videos_to_json(file_in, file_out)
 
-for i in range(1, 1001):
-    file_in = str("../dataset/NonViolence/NV_") + str(i) + ".mp4"
-    file_out = str("../dataset/results/NV_") + str(i) + "_json"
-    convert_videos_to_json(file_in, file_out)
+#for i in range(1, 1001):
+#    file_in = str("../dataset/NonViolence/NV_") + str(i) + ".mp4"
+#    file_out = str("../dataset/results/NV_") + str(i) + "_json"
+#    convert_videos_to_json(file_in, file_out)
 
+import keras
+from keras.layers import Sequential, Dense, LSTM, Activation
+
+
+model = Sequential()
+model.add(LSTM(100, input_shape=(150, 2048))) # 150, 2048 = n_chunks, chunk_size
+model.add(Dense(1024))
+model.add(Activation('relu'))
+model.add(Dense(50))
+model.add(Activation('sigmoid'))
+model.add(Dense(3))
+model.add(Activation('softmax'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+x_train = []
+y_train = []
+epochs = 1500
+batchs = 100
+for i in joint_transfer:
+    x_train.append(i[0])
+    y_train.append(np.array(i[1]))
+
+model.fit(x_train, y_train, epochs, batch_size=batchs, verbose=1)
+model.save("rnn.h5", overwrite=True)
