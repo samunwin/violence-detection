64400cebd24d04ab3c7fb24894fb25341ea23f8c
Szymon Skrzek <skrzekszymon@gmail.com>
2020-03-22 03:03:31 +0000

    updating frame_id loop

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import itertools
+
 from extract import extract_dataset_archive
 from split import convert_train_set_to_images
 import os
@@ -29,7 +31,43 @@ def convert_videos_to_json(path_in, path_out):
 #    convert_videos_to_json(file_in, file_out)
 
 import keras
-from keras.layers import Sequential, Dense, LSTM, Activation
+# from keras.layers import Sequential, Dense, LSTM, Activation
+import pandas as pd
+
+person_count = 5
+
+data=pd.read_csv("dataset/csvs/NV_100_json.csv").to_numpy()
+
+val_cumulative = {}
+frame_ids = set()
+for person in data:
+    frame_ids.add(person[0])
+    frame, person_id = person[:2]
+
+    vel_array = []
+    for vel in range(5, 70, 4):
+        vel_array.append(vel)
+
+    total_vel = sum(vel_array)
+
+    if person_id in val_cumulative:
+        val_cumulative[person_id] += total_vel
+    else:
+        val_cumulative[person_id] = 0
+
+val_cumulative = {k: v for k, v in sorted(val_cumulative.items(), key=lambda item: item[1], reverse=True)}
+top_people = dict(itertools.islice(val_cumulative.items(), person_count))
+top_people_dataset = dict()
+top_people.keys()
+
+for frame_id in frame_ids:
+    top_people_dataset[frame_id] = []
+    people = [item for item in data if item[0] == frame_id]
+    for person in people:
+        if person[1] in top_people:
+            top_people_dataset[person[0]].append(person)
+
+
 
 
 model = Sequential()
